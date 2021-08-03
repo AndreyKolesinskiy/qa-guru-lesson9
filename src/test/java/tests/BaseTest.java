080606a1ae82ce2c44191fe34f319fe1a8714c5b
Andrei Kaliasinski <andykolesinski@gmail.com>
2021-08-03 23:03:26 +0300

    Added selenoid

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.PracticeFormPage;
 
 public class BaseTest {
@@ -14,9 +15,14 @@ public class BaseTest {
 
     @BeforeAll
     static void setUp(){
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.startMaximized = true;
         Configuration.holdBrowserOpen = false;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
     @AfterEach
